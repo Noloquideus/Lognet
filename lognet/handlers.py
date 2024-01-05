d59c50be65b250a8d3f46bab2f0e93428c4aeb15
Noloquideus <daniilmanukian@gmail.com>
2024-01-05 10:29:04 +0300

    feat: (handlers.py) added a mod for writing to a file

--- lognet/handlers.py
+++ lognet/handlers.py
@@ -1,14 +1,17 @@
+import os
+import inspect
 from typing import Optional
 
 
 class FileHandler:
-    def __init__(self, log_file_name: Optional[str] = None):
+    def __init__(self, log_file_name: Optional[str] = None, log_mode: str = 'a'):
         self.log_file_name = log_file_name
-        self.file_handler = open(log_file_name, 'a') if log_file_name else None
+        self.log_mode = log_mode
+        self.file_handler = open(log_file_name, self.log_mode) if log_file_name else None
 
     def write_log(self, log_entry):
         if self.file_handler:
-            with open(self.log_file_name, 'a') as file:
+            with open(self.log_file_name, self.log_mode) as file:
                 file.write(log_entry + '\n')
 
 
