78c6a67ebe098c071fa8c554ce099dea6c7c3dd2
Noloquideus <daniilmanukian@gmail.com>
2024-01-05 12:08:09 +0300

    docs: (handlers.py, logger.py) added documentation

--- lognet/logger.py
+++ lognet/logger.py
@@ -5,6 +5,31 @@ from .singleton import SingletonLogger
 
 
 class Logger(SingletonLogger):
+    """
+    Class Functionality:
+        - Provides a logging interface with customizable log levels, format, and handlers.
+
+        Methods:
+        - __init__: Constructor to initialize the Logger.
+        - set_log_format: Sets a custom log format.
+        - set_file_log_handler: Sets a custom file log handler.
+        - set_console_log_handler: Sets a custom console log handler.
+        - log: Logs a message with the specified log level.
+
+    Example Usage:
+        from my_logger import Logger, ConsoleHandler, LogLevel
+
+        # Creating a logger with custom settings
+        logger = Logger(
+            min_level=LogLevel.INFO,
+            log_format="[INFO] {message} ({file_info})",
+            console_log_handler=ConsoleHandler()
+        )
+
+        # Logging messages
+        logger.log(LogLevel.INFO, "This is an informational message.")
+        logger.log(LogLevel.WARNING, "This is a warning.")
+    """
     def __init__(self, min_level=LogLevel.DEBUG, log_format=None, file_log_handler=None, console_log_handler=None):
         super(Logger, self).__init__()
 
@@ -23,6 +48,13 @@ class Logger(SingletonLogger):
         self.console_log_handler = console_log_handler
 
     def log(self, level, message):
+        """
+        Logs a message with the specified log level.
+
+        Parameters:
+        - level (LogLevel): The log level of the message.
+        - message (str): The log message.
+        """
         if level.value >= self.min_level.value:
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             frame_info = self._get_caller_info()
@@ -41,6 +73,12 @@ class Logger(SingletonLogger):
 
     @staticmethod
     def _get_caller_info():
+        """
+        Retrieves information about the caller (file and line number).
+
+        Returns:
+        - str: Caller information in the format "[file_path:line_number]".
+        """
         frame = inspect.currentframe().f_back
         file_info = f"[{frame.f_globals['__file__']}:{frame.f_lineno}]"
         return file_info
